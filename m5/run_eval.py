951bbb5749877fe244b22f93eaceb860cd190e18
Mohsin Vindhani <mohsinvindhani@gmail.com>
2020-10-31 01:44:54 +0530

    Col name independent

--- m5/run_eval.py
+++ m5/run_eval.py
@@ -8,17 +8,18 @@ import copy
 import os
 import fire
 import glob
+import pdb
 
 ##### import all Feature engineering functions
 from util_feat_m5 import *
 
 
-def features_to_category(df):
-    nan_features = ['event_name_1', 'event_type_1', 'event_name_2', 'event_type_2']
+def features_to_category(df, nan_cols, cat_cols):
+    nan_features = nan_cols
     for feature in nan_features:
         df[feature].fillna('unknown', inplace = True)
 
-    categorical_cols = ['dept_id', 'cat_id', 'store_id', 'state_id', 'event_name_1', 'event_type_1', 'event_name_2', 'event_type_2']
+    categorical_cols = cat_cols
     for feature in categorical_cols:
         encoder     = preprocessing.LabelEncoder()
         df[feature] = encoder.fit_transform(df[feature].astype(str))
@@ -44,29 +45,31 @@ def update_meta_csv(featnames, filename, cat_cols):
     meta_csv.to_csv('meta_features.csv', index = False)
 
 
-def get_cat_num_features_from_meta_csv():
+def get_cat_num_features_from_meta_csv(id_cols, dep_col):
+    drop_cols = id_cols + [dep_col]
     meta_csv = pd.read_csv('meta_features.csv')
-    num_feats = [ x for x in meta_csv[meta_csv["feattype"] == "numeric"]['featname'].tolist()  if x not in ["demand", "date"]]
-    cat_feats = [ x for x in meta_csv[meta_csv["feattype"] == "categorical"]['featname'].tolist() if x not in ["item_id"]]
+    num_feats = [ x for x in meta_csv[meta_csv["feattype"] == "numeric"]['featname'].tolist()  if x not in drop_cols]
+    cat_feats = [ x for x in meta_csv[meta_csv["feattype"] == "categorical"]['featname'].tolist() if x not in drop_cols]
     return cat_feats, num_feats
 
 
-def get_file_feat_from_meta_csv(selected_cols):
+def get_file_feat_from_meta_csv(selected_cols, id_cols):
     meta_csv = pd.read_csv('meta_features.csv')
-    file_feat_mapping = {k:['date', 'item_id'] for k in meta_csv['filename'].unique().tolist()}
+    file_feat_mapping = {k:id_cols for k in meta_csv['filename'].unique().tolist()}
     for selected_col in selected_cols:
         selected_col_meta_df = meta_csv[meta_csv["featname"] == selected_col]
         file_feat_mapping[selected_col_meta_df['filename'].tolist()[0]].append(selected_col)
+    print(id_cols)
     return {k:list(set(v)) for k,v in file_feat_mapping.items()}
 
 
-def features_generate_file(dir_in, dir_out, my_fun_features, features_group_name, input_raw_path = None, max_rows = 10, step_wise_saving = False) :
+def features_generate_file(dir_in, dir_out, my_fun_features, features_group_name, input_raw_path = None, auxiliary_csv_path = None, drop_cols = None, index_cols = None, merge_cols_mapping = None, cat_cols = None, id_cols=None, dep_col = None, max_rows = 5, step_wise_saving = False) :
 
     # from util_feat_m5  import lag_featrues
     # features_generate_file(dir_in, dir_out, lag_featrues)
 
     merged_df = pd.read_parquet(dir_in + "/raw_merged.df.parquet")
-    dfnew, cat_cols= my_fun_features(merged_df, input_raw_path, dir_out, features_group_name, max_rows)
+    dfnew, cat_cols= my_fun_features(merged_df, input_raw_path, dir_out, features_group_name, auxiliary_csv_path, drop_cols, index_cols, merge_cols_mapping, cat_cols, id_cols, dep_col, max_rows)
     if not step_wise_saving:
         dfnew.to_parquet(f'{dir_out}/{features_group_name}.parquet')
     # num_cols = list(set(dfnew._get_numeric_data().columns))
@@ -74,24 +77,26 @@ def features_generate_file(dir_in, dir_out, my_fun_features, features_group_name
 
 
 def feature_merge_df(df_list, cols_join):
+    print(cols_join)
     dfall = None
     for dfi in df_list :
+        print(dfi.columns)
         cols_joini = [ t for t in cols_join if t in dfi.columns ]
         dfall      = dfall.join(dfi.set_index(cols_joini), on = cols_joini, how="left") if dfall is not None else dfi
     return dfall
 
 
-def raw_merged_df(input_path="data/", out_path='out/',  max_rows=10):
-    df_sales_train            = pd.read_csv(input_path + "/sales_train_eval.csv")
-    df_calendar               = pd.read_csv(input_path + "/calendar.csv")
-    df_sales_val              = pd.read_csv(input_path + "/sales_train_val.csv")
-    df_sell_price             = pd.read_csv(input_path + "/sell_prices.csv")
+def raw_merged_df(input_path="data/", out_path='out/',  index_cols = None, dep_col = None, raw_merge_cols = None, merge_cols_mapping = None, nan_cols = None, cat_cols = None, max_rows=10):
+    df_sales_train            = pd.read_csv(input_path + "/sales_train_gen.csv")
+    df_calendar               = pd.read_csv(input_path + "/calendar_gen.csv")
+    df_sales_val              = pd.read_csv(input_path + "/sales_train_gen.csv")
+    df_sell_price             = pd.read_csv(input_path + "/sell_prices_gen.csv")
     # df_submi                  = pd.read_csv("data/sample_submi.csv")
 
     if not max_rows == -1:
-        df_sales_val_melt         = pd.melt(df_sales_val[0:max_rows], id_vars = ['id', 'item_id', 'dept_id', 'cat_id', 'store_id', 'state_id'], var_name = 'day', value_name = 'demand')
+        df_sales_val_melt         = pd.melt(df_sales_val[0:max_rows], id_vars = index_cols, var_name = 'day', value_name = dep_col)
     else:
-        df_sales_val_melt         = pd.melt(df_sales_val, id_vars = ['id', 'item_id', 'dept_id', 'cat_id', 'store_id', 'state_id'], var_name = 'day', value_name = 'demand')
+        df_sales_val_melt         = pd.melt(df_sales_val, id_vars = index_cols, var_name = 'day', value_name = dep_col)
     # val_rows                  = [row for row in df_submi['id'] if 'val' in row]
     # eval_rows                 = [row for row in df_submi['id'] if 'eval' in row]
     # df_submi_val              = df_submi[df_submi['id'].isin(val_rows)][0:max_rows]
@@ -110,10 +115,10 @@ def raw_merged_df(input_path="data/", out_path='out/',  max_rows=10):
     # merged_df = pd.concat([df_sales_val_melt, df_submi_val, df_submi_eval], axis = 0)
     merged_df = df_sales_val_melt
     df_calendar.drop(['weekday', 'wday', 'month', 'year'], inplace = True, axis = 1)
-    merged_df = pd.merge(merged_df, df_calendar, how = 'left', left_on = ['day'], right_on = ['d'])
-    merged_df = merged_df.merge(df_sell_price, on = ['store_id', 'item_id', 'wm_yr_wk'], how = 'left')
+    merged_df = pd.merge(merged_df, df_calendar, how = 'left', left_on = [merge_cols_mapping["left"]], right_on = [merge_cols_mapping["right"]])
+    merged_df = merged_df.merge(df_sell_price, on = raw_merge_cols, how = 'left')
 
-    merged_df = features_to_category(merged_df)
+    merged_df = features_to_category(merged_df, nan_cols = nan_cols, cat_cols = cat_cols)
     # merged_df = add_time_features(merged_df)
 
     os.makedirs(out_path, exist_ok=True)
@@ -124,11 +129,11 @@ def raw_merged_df(input_path="data/", out_path='out/',  max_rows=10):
 
 
 
-def features_get_cols(mode = "random"):
+def features_get_cols(mode = "random", id_cols = None, dep_col = None):
     # categorical_cols = ['item_id', 'dept_id', 'cat_id', 'store_id', 'state_id', 'event_name_1', 'event_type_1', 'event_name_2', 'event_type_2' ]
     # numerical_cols = ['snap_TX',  'sell_price', 'week', 'snap_CA', 'month', 'snap_WI', 'dayofweek', 'year']
 
-    categorical_cols, numerical_cols = get_cat_num_features_from_meta_csv()
+    categorical_cols, numerical_cols = get_cat_num_features_from_meta_csv(id_cols = id_cols, dep_col =dep_col)
 
     cols_cat = []
     cols_num = []
@@ -155,10 +160,12 @@ def get_file_names_to_load_from(file_name, path):
     return flist
 
 
-def load_data(path, selected_cols):
-    selected_cols = ['demand', 'date'] + selected_cols
+def load_data(path, selected_cols, id_cols):
+    print("**********")
+    selected_cols = id_cols + selected_cols
+    print(id_cols)
 
-    file_col_mapping = get_file_feat_from_meta_csv(selected_cols)
+    file_col_mapping = get_file_feat_from_meta_csv(selected_cols = selected_cols, id_cols = id_cols[:])
     # merged_df = pd.DataFrame()
     # for file_name,file_cols in file_col_mapping.items():
     # 	file_df = pd.read_parquet(path + file_name, columns = file_cols)
@@ -169,18 +176,24 @@ def load_data(path, selected_cols):
     # 	print(file_cols)
     # 	pd.read_parquet(f'{path}/{file_name}', columns = file_cols)
 
+    print(id_cols)
+
     feature_dfs = []
     for file_name,file_cols in file_col_mapping.items():
+        print(file_name)
+        print(file_cols)
         file_name_feature_df = None
         for x in get_file_names_to_load_from(file_name, path):
             dfi = pd.read_parquet(f'{x}', columns = file_cols)
             file_name_feature_df = pd.concat((file_name_feature_df, dfi))
         feature_dfs.append(file_name_feature_df)
 
-    merged_df = feature_merge_df(feature_dfs, ['date', 'item_id'])
+    print(id_cols)
+    merged_df = feature_merge_df(feature_dfs, id_cols)
 
     merged_df = merged_df.sort_values('date')
-    merged_df.drop(['item_id'], inplace = True, axis = 1)
+    non_date_col = [x for x in id_cols if not x == "date"]
+    merged_df.drop(non_date_col, inplace = True, axis = 1)
     return merged_df
 
 
@@ -194,7 +207,7 @@ def Y_transform(df, selected_col):
     return Y_df
 
 
-def run_eval(input_path, max_rows = None, n_experiments = 3):
+def run_eval(input_path, max_rows = None, n_experiments = 3, id_cols = None, dep_col = None):
     model_params = {'num_leaves': 555,
           'min_child_weight' : 0.034,
           'feature_fraction' : 0.379,
@@ -212,11 +225,13 @@ def run_eval(input_path, max_rows = None, n_experiments = 3):
           'random_state'     : 222,
          }
 
+    print("hello eval")
+
     dict_metrics = {'run_id' : [], 'cols' : [], 'metric_name': [], 'model_params': [], 'metrics_val' : []}
 
     for ii in range(n_experiments):
-        cols_cat, cols_num   = features_get_cols()
-        df          		 = load_data(input_path, cols_cat + cols_num)
+        cols_cat, cols_num   = features_get_cols(id_cols = id_cols, dep_col = dep_col)
+        df          		 = load_data(input_path, cols_cat + cols_num + [dep_col], id_cols)
 
         # df_output            = load_data('data/output', cols_cat + cols_num, 'test1')
 
@@ -254,12 +269,13 @@ def run_eval(input_path, max_rows = None, n_experiments = 3):
 
 
 
-def generate_feature_all( input_path = "data/output", out_path="." , input_raw_path = ".", max_rows = 10):
-    features_generate_file(".", input_path, features_time_basic, "basic_time")
-    features_generate_file(".", input_path, features_rolling, "rolling")
-    features_generate_file(".", input_path, features_lag, "lag")
-    features_generate_file(".", input_path, features_tsfresh, "tsfresh", input_raw_path, max_rows, step_wise_saving = True)
-    features_generate_file(".", input_path, identity_features, "identity")
+def generate_feature_all( input_path = "data/output", out_path="." , input_raw_path = ".", auxiliary_csv_path = None, drop_cols = None, index_cols = None, merge_cols_mapping = None, cat_cols = None, id_cols = None, dep_col = None, max_rows = 10):
+    features_generate_file(".", input_path, features_time_basic, "basic_time", id_cols = id_cols)
+    features_generate_file(".", input_path, features_rolling, "rolling", dep_col = dep_col, id_cols = id_cols)
+    features_generate_file(".", input_path, features_lag, "lag", dep_col = dep_col, id_cols = id_cols)
+    features_generate_file(".", input_path, features_tsfresh, "tsfresh", input_raw_path, auxiliary_csv_path, drop_cols, index_cols, merge_cols_mapping, max_rows, step_wise_saving = True, id_cols = id_cols)
+    features_generate_file(".", input_path, identity_features, "identity", cat_cols = cat_cols, drop_cols = ['d', 'id', 'day', 'wm_yr_wk'])
+    print("hello")
 
 
 def main( input_path = "data/output", out_path="",
@@ -271,13 +287,17 @@ def main( input_path = "data/output", out_path="",
     # To be run once
 
     if do_generate_raw :
-      raw_merged_df(input_path= input_path, out_path=out_path, max_rows = max_rows)
+      raw_merged_df(input_path= input_path, out_path=out_path, max_rows = max_rows, index_cols =
+      [ 'id', 'cat_id_col', 'dept_id_col', 'store_id_col', 'item_id_col', 'state_id_col'], dep_col = "demand",
+      raw_merge_cols = ['store_id_col', 'item_id_col', 'wm_yr_wk'], merge_cols_mapping = {"left" : "day", "right" : "d"},
+      nan_cols = ['event_name_1_col', 'event_type_1_col', 'event_name_2_col', 'event_type_2_col'],
+      cat_cols = ['dept_id_col', 'cat_id_col', 'store_id_col', 'state_id_col', 'event_name_1_col', 'event_type_1_col', 'event_name_2_col', 'event_type_2_col'])
 
     if do_generate_feature :
-      generate_feature_all(input_path="data/output", out_path="", input_raw_path = input_path, max_rows = max_rows)
+      generate_feature_all(input_path="data/output", out_path="", input_raw_path = input_path + "/sales_train_gen.csv", auxiliary_csv_path = input_path + "/calendar_gen.csv", drop_cols = [ 'id', 'cat_id_col', 'dept_id_col', 'store_id_col', 'variable', 'day', 'demand', 'state_id_col', 'weekday', 'wday', 'month', 'year'], index_cols = [ 'id', 'cat_id_col', 'dept_id_col', 'store_id_col', 'item_id_col', 'state_id_col'], merge_cols_mapping = {"left" : "day", "right" : "d"}, cat_cols = ['item_id_col', 'dept_id_col', 'cat_id_col', 'store_id_col', 'state_id_col', 'event_name_1_col', 'event_type_1_col', 'event_name_2_col', 'event_type_2_col'], id_cols = ["date", "item_id_col"], dep_col = "demand", max_rows = max_rows)
 
 
-    run_eval(input_path="data/output")
+    run_eval(input_path="data/output", id_cols = ["date", "item_id_col"], dep_col = "demand")
 
 
 if __name__ == "__main__":
